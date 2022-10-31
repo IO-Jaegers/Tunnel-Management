79de6b065ae220d748f0cf9a6fe95e553544b1be
madsen, kent v <kent.vejrup.madsen@outlook.com>
2022-10-31 12:43:37 +0100

    added files to be ignored

--- scripts/wireguard/configuration/template.py
+++ scripts/wireguard/configuration/template.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-extension_name = '.conf'
+from typing import Final
+
+var_extension_name = '.conf'
+
+class variables:
+    def __init__( self ):
+        global var_extension_name
+        extension_name: Final = var_extension_name
 
 
 # generates a generic template that has to be filled out by a user.
 # gives a nice starting point.
 def generic( name ):
+    defaults = variables()
 
-    pass
+    
