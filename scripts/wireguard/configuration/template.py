9fc50617896214185ce2475419456ca5906baa33
madsen, kent v <kent.vejrup.madsen@outlook.com>
2022-10-31 12:08:03 +0100

    added variables

---
+++ scripts/wireguard/configuration/template.py
@@ -0,0 +1,29 @@
+extension_name = '.conf'
+
+var_addr = 'Address'
+var_dns = 'DNS'
+
+var_private_key = 'PrivateKey'
+var_public_key = 'PublicKey'
+var_shared_key = 'PresharedKey'
+
+var_persistent_keepalive = 'PersistentKeepalive'
+var_endpoint = 'Endpoint'
+var_allowed_ips = 'AllowedIPs'
+
+var_listen_ports = 'ListenPort'
+
+var_post_up = 'PostUp'
+var_post_down = 'PostDown'
+
+var_save_config = 'SaveConfig'
+
+var_attribute_interface = '[Interface]'
+var_attribute_peer = '[Peer]'
+
+
+# generates a generic template that has to be filled out by a user.
+# gives a nice starting point.
+def generic( name ):
+
+    pass
